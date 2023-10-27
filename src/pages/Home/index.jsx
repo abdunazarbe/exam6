6f2567c53b27b987023230135db314319ce11428
unknown <abu040810@gmail.com>
2023-10-28 03:09:29 +0500

    first c

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,18 @@
+import {useEffect} from "react";
+import {Button} from "flowbite-react";
+import Layout from "../../components/Layout"
+
+
+const index = () => {
+
+  
+
+    return (
+        <div>
+            <h1>Home</h1>
+            <Layout></Layout>
+        </div>
+    );
+};
+
+export default index;
