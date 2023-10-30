0e931e8cc4076de777fc8e229201c3f793d58510
unknown <abu040810@gmail.com>
2023-10-30 16:00:51 +0500

    ok

--- src/Router/index.jsx
+++ src/Router/index.jsx
@@ -1,10 +1,14 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import { createBrowserRouter } from "react-router-dom";
-
 import Layout from "../pages/Layout/Layout";
+import Layout1 from "../Dashboard/Layout";
 import Home from "../pages/Home";
-import About from "../pages/About";
+import Students from "../Dashboard/Oquvchilar";
+import Dhome from "../Dashboard/Dhome";
+import Kurs from "../Dashboard/Kurslar";
+import Buyurtmachilar from "../Dashboard/Buyurtmachilar";
+import Xizmatlar from "../Dashboard/Xizmatlar";
 import NotFound from "../pages/NotFound";
 
 const router = createBrowserRouter([
@@ -17,9 +21,33 @@ const router = createBrowserRouter([
         path: "/",
         element: <Home />,
       },
+    ],
+  },
+  {
+    path: "/dashboard",
+    element: <Layout1 />,
+    errorElement: <NotFound />,
+    children: [
       {
-        path: "/about",
-        element: <About />,
+        path: "/dashboard",
+        element: <Dhome />,
+      },
+      {
+        path: "/dashboard/oquvchilar",
+        element: <Students />,
+      },
+      {
+        path: "/dashboard/kurslar",
+        element: <Kurs />,
+      },
+      {
+        path: "/dashboard/buyurtmachilar",
+        element: <Buyurtmachilar />,
+      },
+
+      {
+        path: "/dashboard/xizmatlar",
+        element: <Xizmatlar />,
       },
     ],
   },
